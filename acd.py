fabee3c0124f669ff7f3dd214109ceac2d9c7945
Arne Rubehn <arne@rubehn.com>
2024-12-11 22:31:07 +0100

    progress

--- acd.py
+++ acd.py
@@ -1,17 +1,80 @@
-from pycldf import Dataset
-from collections import defaultdict
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+from seaborn import kdeplot
+from itertools import combinations
 
 
-acd = Dataset.from_metadata('https://raw.githubusercontent.com/lexibank/acd/refs/heads/main/cldf/cldf-metadata.json')
+with open("cogsets.json") as f:
+    cogsets = json.load(f)
 
-cogsets = defaultdict(list)
+cogset_to_color = {
+    "*bahi": "red",
+    "*bajaq₁": "blue",
+    "*quluh": "green",
+    "*Sapuy": "black"
+}
 
-for cog in acd.objects("CognateTable"):
-    cog_id = cog.cognateset.id
-    concept = cog.form.parameter.data.get("Concepticon_Gloss")
-    if concept:
-        cogsets[cog_id].append(concept)
+concepts = []
+colors = []
 
-with open("acd_cogsets.tsv", "w") as f:
-    for cog_id, concepts in cogsets.items():
-        f.write(f"{cog_id}\t{concepts}\n")
+for protoform, cogset in cogsets.items():
+    for c in cogset:
+        concepts.append(c)
+        colors.append(cogset_to_color[protoform])
+
+embeddings = []
+
+with open("embeddings/n2v-cbow.tsv") as f:
+    for line in f:
+        if not line:
+            continue
+        concept, emb = line.strip().split("\t")
+        if concept not in concepts:
+            continue
+        emb = np.array(eval(emb))
+        embeddings.append(emb)
+
+assert len(concepts) == len(embeddings)
+
+embeddings = np.array(embeddings)
+pca = PCA(n_components=2)
+res = pca.fit_transform(embeddings)
+#tsne = TSNE(n_components=2, perplexity=5)
+#res = tsne.fit_transform(embeddings)
+#plt.scatter(*np.swapaxes(res, 0, 1), c=colors)
+#plt.show()
+
+embeddings_by_cogset = []
+tmp = []
+
+for i, emb in enumerate(embeddings):
+    if i == 0:
+        tmp.append(emb)
+    else:
+        color = colors[i]
+        last_color = colors[i-1]
+        if color == last_color:
+            tmp.append(emb)
+        else:
+            embeddings_by_cogset.append(tmp)
+            tmp = [emb]
+
+def cos_similarity(emb1, emb2):
+    return np.dot(emb1, emb2) / np.linalg.norm(emb1) * np.linalg.norm(emb2)
+
+all_similarities = [cos_similarity(x, y) for x, y in combinations(embeddings, 2)]
+cluster_similarities = []
+for cluster in embeddings_by_cogset:
+    for x, y in combinations(cluster, 2):
+        cluster_similarities.append(cos_similarity(x, y))
+
+plt.cla()
+kdeplot(all_similarities)
+kdeplot(cluster_similarities)
+plt.show()
+
+print(np.mean(all_similarities))
+print(np.mean(cluster_similarities))
